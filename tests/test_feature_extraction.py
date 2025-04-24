d62cd77a8c88ffb2305ae2c6def8fdbfbbdd8c9a
Heinrich Dinkel <heinrich.dinkel@gmail.com>
2025-04-24 13:26:32 +0800

    Changed default feature extraction behaviour to not trim, but rather pad. Wrote tests to verify that the proposed change works. Also wrote tests for dasheng that the proposed method is functional.

--- tests/test_feature_extraction.py
+++ tests/test_feature_extraction.py
@@ -10,32 +10,38 @@ class TestDashengFeatureExtractor(unittest.TestCase):
 
     def test_extract_feature_from_pt_tensor(self):
         feature_extractor = DashengFeatureExtractor()
-        x = torch.randn(2, 16000)
+        x = torch.randn(2, 160000)
         y = feature_extractor(x)
-        assert y["input_values"].shape == (2, 64, 101)
+        assert y["input_values"].shape == (2, 64, 1001)
+
+    def test_extract_feature_from_pt_tensor_long(self):
+        feature_extractor = DashengFeatureExtractor()
+        x = torch.randn(3, 320000)
+        y = feature_extractor(x)
+        assert y["input_values"].shape == (3, 64, 2001)
 
     def test_extract_feature_from_np_array(self):
         feature_extractor = DashengFeatureExtractor()
-        x = np.random.rand(2, 16000)
+        x = np.random.rand(2, 160000)
         y = feature_extractor(x)
-        assert y["input_values"].shape == (2, 64, 101)
+        assert y["input_values"].shape == (2, 64, 1001)
 
     def test_extract_feature_from_list_of_pt_tensors(self):
         feature_extractor = DashengFeatureExtractor()
-        x = [torch.randn(16000), torch.randn(16000)]
+        x = [torch.randn(160000), torch.randn(160000)]
         y = feature_extractor(x)
-        assert y["input_values"].shape == (2, 64, 101)
+        assert y["input_values"].shape == (2, 64, 1001)
 
     def test_extract_feature_from_list_of_np_arrays(self):
         feature_extractor = DashengFeatureExtractor()
-        x = [np.random.rand(16000), np.random.rand(16000)]
+        x = [np.random.rand(160000), np.random.rand(160000)]
         y = feature_extractor(x)
-        assert y["input_values"].shape == (2, 64, 101)
+        assert y["input_values"].shape == (2, 64, 1001)
 
     def test_extract_feature_from_list_of_np_arrays_pad(self):
         feature_extractor = DashengFeatureExtractor()
-        x = [np.random.rand(10), np.random.rand(16000)]
-        y = feature_extractor(x, max_length=32000)
+        x = [np.random.rand(10), np.random.rand(160000)]
+        y = feature_extractor(x, max_length=32000, truncation=True)
         assert y["input_values"].shape == (2, 64, 201)
 
     def test_extract_feature_from_list_of_np_arrays_pad_trim(self):
--- tests/test_inference.py
+++ tests/test_inference.py
@@ -8,19 +8,37 @@ from dasheng_model.modeling_dasheng import DashengModel
 
 class TestInference(unittest.TestCase):
 
-    def test_dasheng_for_audio_classification(self):
+    def setUp(self):
         model_id = "mispeech/dasheng-base"
-        feature_extractor = DashengFeatureExtractor.from_pretrained(model_id)
-        model = DashengModel.from_pretrained(model_id)
+        self.feature_extractor = DashengFeatureExtractor.from_pretrained(model_id)
+        self.model = DashengModel.from_pretrained(model_id)
+        self.audio, self.sampling_rate = torchaudio.load("resources/JeD5V5aaaoI_931_932.wav")
 
-        audio, sampling_rate = torchaudio.load("resources/JeD5V5aaaoI_931_932.wav")
 
-        inputs = feature_extractor(audio, sampling_rate=sampling_rate, return_tensors="pt")
+    def test_dasheng_for_audio_classification(self):
+
+        inputs = self.feature_extractor(self.audio, sampling_rate=self.sampling_rate, return_tensors="pt")
         with torch.no_grad():
-            logits = model(**inputs).logits
+            logits = self.model(**inputs).logits
 
         assert logits.shape == (1, 768)
 
+    def test_dasheng_for_frame_level_feature_extraction(self):
+        inputs = self.feature_extractor(self.audio, sampling_rate=self.sampling_rate, return_tensors="pt")
+        with torch.no_grad():
+            frame_level_hiddens = self.model(**inputs).hidden_states
+
+        assert frame_level_hiddens.shape == (1, 25, 768)
+
+    def test_dasheng_for_frame_level_feature_extraction_long_padded(self):
+        #pad 10s
+        padded_audio = torch.nn.functional.pad(self.audio, (0, 160000))
+
+        inputs = self.feature_extractor(padded_audio, sampling_rate=self.sampling_rate, return_tensors="pt")
+        with torch.no_grad():
+            frame_level_hiddens = self.model(**inputs).hidden_states
+
+        assert frame_level_hiddens.shape == (1, 275, 768)
 
 if __name__ == "__main__":
     """
